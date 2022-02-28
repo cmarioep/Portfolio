ada7fdc48c5b658bd9c095cf3ba1889db19a7c6c
cmarioep <cmescandonpardo@gmail.com>
2022-02-28 00:25:45 -0500

    Feat: Dynamic alternated flow for BigCards on projects section

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 import GradientTitle from "../components/GradientTitle";
 import BigCard from "../components/BigCard";
@@ -6,43 +7,59 @@ import SmallCard from "../components/SmallCard";
 
 // styles
 import '../styles/layout/_layout.scss';
+import '../styles/components/_cards.scss';
 
 
 const projects = [
   {
-    title : "Titulo 1",
-    description : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique mollitia consequatur nam quidem necessitatibus dolorem illo, assumenda eligendi ipsam pariatur at beatae debitis soluta a sit totam quod consectetur accusantium maxime? Harum natus perferendis adipisci."
+    title: "Titulo 1",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique mollitia consequatur nam quidem necessitatibus dolorem illo, assumenda eligendi ipsam pariatur at beatae debitis soluta a sit totam quod consectetur accusantium maxime? Harum natus perferendis adipisci."
   }
 ]
 
 
 export default function Projects(props) {
+
+
+  useEffect(() => {
+    const oddItemsList = document.querySelectorAll(".bigCard:nth-child(odd)");
+    const oddItemsArray = [...oddItemsList];
+
+    oddItemsArray.map(item => item.classList.add("bigCards--alternatedFlow"));
+    
+  });
+
+
   return (
     <section className='ly-viewport'>
 
-        <GradientTitle title="Projects"/>
+      <GradientTitle title="Projects" />
 
-        <div className="ly-blockFlow">
-          <BigCard
-            title={projects[0].title}
-            description={projects[0].description}
-          />
+      <div className="ly-blockFlow alternatedCardsFlow">
+        <BigCard
+          title={projects[0].title}
+          description={projects[0].description}
+        />
 
-          <BigCard
-            align = "--mirror"
-            title={projects[0].title}
-            description={projects[0].description}
-          />
-        </div>
+        <BigCard
+          title={projects[0].title}
+          description={projects[0].description}
+        />
+ 
+        <BigCard
+          title={projects[0].title}
+          description={projects[0].description}
+        />
+      </div>
 
-        {/* sytle for h2 are defined on styles/base/_base.scss */}
-        <h2>Other Noteworthy Projects</h2>
-  
-        <div className="ly-inlineFlow">
-          <SmallCard />
-          <SmallCard />
-          <SmallCard />
-        </div>
+      {/* sytle for h2 are defined on styles/base/_base.scss */}
+      <h2>Other Noteworthy Projects</h2>
+
+      <div className="ly-inlineFlow">
+        <SmallCard />
+        <SmallCard />
+        <SmallCard />
+      </div>
 
     </section>
   );
