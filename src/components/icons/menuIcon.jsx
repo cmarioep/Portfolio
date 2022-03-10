9ab51ff5707bf63b7b1ad4756350cac5712cfd4e
cmarioep <cmescandonpardo@gmail.com>
2022-03-09 21:24:16 -0500

    Feat: svg icons components for navBar

---
+++ src/components/icons/menuIcon.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const menuIcon = (props) => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path d="M0 7.5v5h50v-5Zm0 15v5h50v-5Zm0 15v5h50v-5Z" />
+  </svg>
+);
+
+
+export default menuIcon;
