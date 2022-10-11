887c9806756f4deeaecae3884a96c64d10cdc348
cmarioep <cmescandonpardo@gmail.com>
2022-10-11 16:09:06 -0500

    Update: breaking chanche migration from gatsby 4.6.2 to gatsby 4.24.4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,20 @@
 module.exports = {
   siteMetadata: {
-      title: `My Gatsby Porfolio`,
+    title: `New`,
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
     "gatsby-plugin-sass",
-    `gatsby-plugin-image`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-sharp`,
-  ]
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }]
 };
