5379dd5be31aa04ed0259a50ac881009204ac082
cmarioep <cmescandonpardo@gmail.com>
2022-11-28 16:32:44 -0500

    Refactor: BigCards on Projects

--- src/components/ProjectScreenshot.js
+++ src/components/ProjectScreenshot.js
@@ -5,15 +5,15 @@ export default function ProjectScreenshot({image}) {
 
     
         if (image === 'Project-1') {
-            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-1.png" alt="Project Screenshot" /> 
+            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-1.jpg" alt="Project Screenshot" /> 
         }
         
         if (image === 'Project-2') {
-            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-2.webp" alt="Project Screenshot" /> 
+            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-2.jpg" alt="Project Screenshot" /> 
         }  
 
         if (image === 'Project-3') {
-            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-3.png" alt="Project Screenshot" /> 
+            return <StaticImage className="bigCard__screenshot__image" src="../images/Project-3.jpg" alt="Project Screenshot" /> 
         }  
 
 }
--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -14,23 +14,23 @@ const projects = [
   {
     id: 'Project-1',
     title: 'CSS Code Generator',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique mollitia consequatur nam quidem necessitatibus dolorem illo, assumenda eligendi ipsam pariatur at beatae debitis soluta a sit totam quod consectetur accusantium maxime? Harum natus perferendis adipisci.',
+    description: 'CSS code generator that provides visual help to obtain the desired effects, works for box-shadow, text-shadow and Glass Morphism.',
     repository: 'https://github.com/cmarioep/CSS-Generator',
     link: 'https://box-shadow-generator-pi.vercel.app/',
   },
   {
     id: 'Project-2',
     title: 'Gif Browser',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique mollitia consequatur nam quidem necessitatibus dolorem illo, assumenda eligendi ipsam pariatur at beatae debitis soluta a sit totam quod consectetur accusantium maxime? Harum natus perferendis adipisci.',
-    repository: 'https://github.com/cmarioep/CSS-Generator',
-    link: 'https://box-shadow-generator-pi.vercel.app/',
+    description: 'A simple Gif Browser that connects to Giphy API and brings a list of gifs searched by user.',
+    repository: 'https://github.com/cmarioep/gif-browser',
+    link: 'https://gif-browser-green.vercel.app/',
   },
   {
     id: 'Project-3',
     title: 'ROD Consulting',
-    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique mollitia consequatur nam quidem necessitatibus dolorem illo, assumenda eligendi ipsam pariatur at beatae debitis soluta a sit totam quod consectetur accusantium maxime? Harum natus perferendis adipisci.',
-    repository: 'https://github.com/cmarioep/CSS-Generator',
-    link: 'https://box-shadow-generator-pi.vercel.app/',
+    description: 'Landing page for electrical engineering consulting company, build with vanilla JavaScript, Bootstrap 5 and custom styles with Sass.',
+    repository: 'https://github.com/cmarioep/ROD-Consulting',
+    link: 'https://cmarioep.github.io/ROD-Consulting/',
   }
 ]
 
@@ -53,32 +53,16 @@ export default function Projects() {
       <GradientTitle title="Projects" />
 
       <div className="ly-blockFlow">
-        <BigCard
-          id={projects[0].id}
-          title={projects[0].title}
-          screenshot={projects[0].screenshot}
-          description={projects[0].description}
-          repository = {projects[0].repository}
-          link = {projects[0].link}
-        />
 
-        <BigCard
-          id={projects[1].id}
-          title={projects[1].title}
-          screenshot={projects[1].screenshot}
-          description={projects[1].description}
-          repository = {projects[1].repository}
-          link = {projects[1].link}
-        />
- 
-        <BigCard
-          id={projects[2].id}
-          title={projects[2].title}
-          screenshot={projects[2].screenshot}
-          description={projects[2].description}
-          repository = {projects[2].repository}
-          link = {projects[2].link}
-        />
+        {
+          projects.map(project => 
+            <BigCard
+              key={project.id}
+              {...project}
+            />
+          )
+        }
+
       </div>
 
       {/* sytle for h2 are defined on styles/base/_base.scss */}
