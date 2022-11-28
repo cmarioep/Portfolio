32d8c7c7d426209abf8faad151cc05ff95a62468
cmarioep <cmescandonpardo@gmail.com>
2022-11-28 18:52:21 -0500

    Style: footer link

--- src/sections/Footer.js
+++ src/sections/Footer.js
@@ -6,8 +6,7 @@ import '../styles/components/_footer.scss';
 export default function Footer() {
   return (
     <footer className="footer">
-      {/* <span>{'\u2764'}</span> */}
-        <p>Designed & Made with love by Mario © 2022</p>
+              <p>Designed & Made with <span>{'\u2764'}</span> by <a href="https://github.com/cmarioep/Portfolio" target="_blank" rel="noreferrer">cmarioep</a> © 2022</p>
         <p>Built with Gatsby</p>
     </footer>
   );
