28163b578ebd8982ec9b161a400354694396fe4d
cmarioep <cmescandonpardo@gmail.com>
2023-05-23 00:38:26 -0500

    Update: 2023 at footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -6,7 +6,7 @@ import '../styles/components/_footer.scss';
 export function Footer() {
   return (
     <footer className="footer">
-      <p>Designed & Made with <span>{'\u2764'}</span> by <a href="https://github.com/cmarioep/Portfolio" target="_blank" rel="noreferrer">cmarioep</a> © 2022</p>
+      <p>Designed & Made with <span>{'\u2764'}</span> by <a href="https://github.com/cmarioep/Portfolio" target="_blank" rel="noreferrer">cmarioep</a> © 2023</p>
       <p>Built with Gatsby</p>
     </footer>
   );
